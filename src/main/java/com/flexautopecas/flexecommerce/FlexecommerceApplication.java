adff4f9e79122f7bfd1823367731bd4667f9fc26
Luan de Oliveira <luan012012@hotmail.com>
2024-01-28 11:57:17 -0300

    Projeto criado

---
+++ src/main/java/com/flexautopecas/flexecommerce/FlexecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.flexautopecas.flexecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flexautopecas/flexecommerce/FlexecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexautopecas.flexecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
