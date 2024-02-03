49c67b519f3d81b09c4e76630ccfb21f1d6305af
Luan de Oliveira <luan012012@hotmail.com>
2024-02-02 23:29:20 -0300

    Criado findById para category

--- src/main/java/com/flexautopecas/flexecommerce/resources/CategoryResource.java
+++ src/main/java/com/flexautopecas/flexecommerce/resources/CategoryResource.java
@@ -3,9 +3,11 @@ package com.flexautopecas.flexecommerce.resources;
 import com.flexautopecas.flexecommerce.dto.CategoryDTO;
 import com.flexautopecas.flexecommerce.entities.Category;
 import com.flexautopecas.flexecommerce.services.CategoryService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,11 @@ public class CategoryResource {
     public ResponseEntity<List<CategoryDTO>> findAll(){
         List<CategoryDTO> categories = categoryService.findAll();
         return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+        CategoryDTO category = categoryService.findByID(id);
+        return ResponseEntity.ok(category);
     }
 }
--- src/main/java/com/flexautopecas/flexecommerce/services/CategoryService.java
+++ src/main/java/com/flexautopecas/flexecommerce/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.flexautopecas.flexecommerce.services;
 import com.flexautopecas.flexecommerce.dto.CategoryDTO;
 import com.flexautopecas.flexecommerce.entities.Category;
 import com.flexautopecas.flexecommerce.repositories.CategoryRepository;
+import com.flexautopecas.flexecommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,5 +21,12 @@ public class CategoryService {
     public List<CategoryDTO> findAll(){
         List<Category> categories = repository.findAll();
         return categories.stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findByID(Long id){
+        Optional<Category> optional = repository.findById(id);
+        Category category = optional.orElseThrow(() -> new ResourceNotFoundException("Categoria não encontrada"));
+        return new CategoryDTO(category);
     }
 }
