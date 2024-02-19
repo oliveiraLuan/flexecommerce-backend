135a012de1f03fb93d90b0ca151787c8155ad2f0
Luan de Oliveira <luan012012@hotmail.com>
2024-02-18 23:10:31 -0300

    Teste Delete ProductRepository

---
+++ src/test/java/com/flexautopecas/flexecommerce/repositories/ProductRepositoryTests.java
@@ -0,0 +1,25 @@
+package com.flexautopecas.flexecommerce.repositories;
+
+import com.flexautopecas.flexecommerce.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    public void deleteShouldDeleteProductWhenIdExists(){
+        Long existingId = 1L;
+        repository.deleteById(1L);
+
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertFalse(result.isPresent());
+    }
+}
