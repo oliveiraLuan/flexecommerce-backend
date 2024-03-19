74fdf41493dcb00da271c5ce80fb3e6e2211ef8d
Luan de Oliveira <luan012012@hotmail.com>
2024-03-19 08:38:15 -0300

    feat: Adicionado entidades User e Role

---
+++ src/main/java/com/flexautopecas/flexecommerce/entities/Role.java
@@ -0,0 +1,45 @@
+package com.flexautopecas.flexecommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String authority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
