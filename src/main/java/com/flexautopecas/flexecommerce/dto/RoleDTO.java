578f3876749e72ca3b65909f438e32202cd06d58
Luan de Oliveira <luan012012@hotmail.com>
2024-03-21 23:13:59 -0300

    feat: adicionado dtos para crud de usuarios

---
+++ src/main/java/com/flexautopecas/flexecommerce/dto/RoleDTO.java
@@ -0,0 +1,26 @@
+package com.flexautopecas.flexecommerce.dto;
+
+public class RoleDTO {
+    private Long id;
+    private String authority;
+    public RoleDTO(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
