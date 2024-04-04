daf54b37f27b57b3181bc5f01f74ebaac7509423
Luan de Oliveira <luan012012@hotmail.com>
2024-04-03 22:48:25 -0300

    feat: adicionado métodos hasRole e addRole

--- src/main/java/com/flexautopecas/flexecommerce/entities/User.java
+++ src/main/java/com/flexautopecas/flexecommerce/entities/User.java
@@ -76,6 +76,19 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public void addRole(Role role){
+        roles.add(role);
+    }
+
+    public Boolean hasRole(String roleName){
+        for(Role role : roles){
+            if(role.getAuthority().equals(roleName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
